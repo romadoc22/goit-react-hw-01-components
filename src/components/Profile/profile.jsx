8310a0e807bf8a11af0589ffbe4c5695d4b43a2d
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-04-27 00:15:40 +0300

    1 завдання

---
+++ src/components/Profile/profile.jsx
@@ -0,0 +1,37 @@
+import user from 'parts/user.json';
+import css from 'components/Profile/profile.module.css';
+
+export const Profile = () => {
+  return (
+    <div className={css.profile}>
+      <div className="description">
+        <img
+          src={user.avatar}
+          alt="User avatar"
+          className={css.avatar}
+          width="200px"
+        />
+        <p className={css.name}>{user.username}</p>
+        <p className={css.tag}>{user.tag}</p>
+        <p className={css.location}>{user.location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{user.stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{user.stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{user.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+console.log(Profile);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-
+// import PropTypes from 'prop-types';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// const elem1 = <span>Hello</span>;
+// console.log(elem1);
+
+// const element = React.createElement('div', {
+//   a: 5,
+//   b: 7,
+//   children: 'Привіт світ',
+// });
+// console.log(element);
+
+// const jsxElement = <div>qqwe 'Hello' {elem1}erywtgrv</div>;
+// console.log(jsxElement);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(jsxElement);
+// const data = {
+//   username: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   location: 'Ocho Rios, Jamaica',
+//   avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+
+// ReactDOM.createRoot(document.getElementById('root')).render(profile);
