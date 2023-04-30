88b784401dc7b9020447c7301247f3a6e7059fd7
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-04-30 10:10:54 +0300

    Update FriendList.styled.jsx

--- src/components/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList.styled.jsx
@@ -12,6 +12,7 @@ export const Li = styled.li`
   flex-basis: auto;
   flex-basis: 125px;
   border: 3px solid rgba(0, 0, 0, 0.2);
+  background-color: #a1f1d5;
 `;
 
 export const Status = styled.span`
